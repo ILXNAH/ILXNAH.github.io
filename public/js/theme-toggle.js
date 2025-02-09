89b9e2ce8407c87546dcc04149de793db0c0226a
ILXNAH <ILXNAH@tutanota.com>
2025-02-09 11:08:57 +0100

    Blog Update on 2025-02-09 11:08:57

--- public/js/theme-toggle.js
+++ public/js/theme-toggle.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("Theme toggle script loaded");
-  const themeToggleButton = document.getElementById('theme-toggle');
-  console.log('Button found:', themeToggleButton); // Check if the button is found
-  let currentTheme = localStorage.getItem('theme') || 'default'; // Retrieve theme from local storage if it exists
+  // Log when the theme toggle script is loaded
+  // console.log("Theme toggle script loaded");
 
-  // Set initial theme on page load
+  const themeToggleButton = document.getElementById('theme-toggle');
+  // Check if the theme toggle button is found
+  // console.log('Button found:', themeToggleButton);
+  
+  // Get the current theme from localStorage or default to 'default'
+  let currentTheme = localStorage.getItem('theme') || 'default';
+
+  // Set the theme on page load, before the page is rendered
   document.documentElement.setAttribute('data-theme', currentTheme);
 
   // Toggle theme when button is clicked
   themeToggleButton.addEventListener('click', function() {
     console.log('Button clicked!'); // Check if button click is firing
+
+    // Cycle through the themes
     let newTheme;
     if (currentTheme === 'default') {
       newTheme = 'pink';
