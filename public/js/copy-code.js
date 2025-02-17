8cb6ea621df5eaf143863cebd2805f8a56ae6522
ILXNAH <ILXNAH@tutanota.com>
2025-02-17 12:58:29 +0100

    Blog Update on 2025-02-17 12:58:29

--- public/js/copy-code.js
+++ public/js/copy-code.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   codeBlocks.forEach(function(container) {
     const button = document.createElement('button');
     button.classList.add('copy-code');
-    button.innerText = 'Copy Text';
+    button.innerText = 'Copy Code';
     container.appendChild(button);
 
     // Add click event to copy the code
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
       // Optional: show some feedback (e.g., "Copied!" message)
       button.innerText = 'Copied!';
       setTimeout(() => {
-        button.innerText = 'Copy Text';
+        button.innerText = 'Copy Code';
       }, 2000);
     });
   });
